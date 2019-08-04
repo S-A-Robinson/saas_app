90baf164d7655d074d031fbe3b6d98e56420137a
Scott Robinson <scott.robinson4477@gmail.com>
2019-08-04 15:55:17 +0000

    added create action to save contact object to database

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,20 @@
 class ContactsController < ApplicationController
    def new
+      #A new contact object will be created
+      @contact = Contact.new
    end
+   
+   def create
+      @contact = Contact.new(contact_params)
+      if @contact.save
+         redirect_to new_contact_path, notice: "Message sent."
+      else
+         redirect_to new_contact_path, notice: "Error sending message, please try again."
+      end
+   end
+   
+   private
+      def contact_params
+         params.require(:contact).permit(:name, :email, :comments)
+      end
 end
