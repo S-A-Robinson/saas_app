6788327cf4061e6cafbdd6b40be97e840f3bbae6
Scott Robinson <scott.robinson4477@gmail.com>
2019-08-04 23:15:52 +0000

    added form validation to contact form model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,6 @@
 class Contact < ActiveRecord::Base
-    
+    #Adds validation to the contact form so they must be filled in to be accepted into the database
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :comments, presence: true
 end
