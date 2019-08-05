3ed99e564c0f4b00f6aa56a19dc1ff363d054472
Scott Robinson <scott.robinson4477@gmail.com>
2019-08-05 10:49:15 +0000

    Cleaned up contact page routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
     #root is the home page of the site
     root to: 'pages#home'
     get 'about', to: 'pages#about'
-    resources :contacts
+    resources :contacts, only: :create
+    get "contact-us", to: "contacts#new", as: "new_contact"
 end
